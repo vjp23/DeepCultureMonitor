180ccd8385db0a14402d4105173fbab0ceb561e0
Vince Petaccio <vincepetaccio@gmail.com>
2023-03-31 15:04:32 -0400

    Simplify IP publication loop Dockerfile

--- start_budbucket_ip_loop.sh
+++ start_budbucket_ip_loop.sh
@@ -13,7 +13,7 @@ fi
 docker stop ip-loop
 docker rm ip-loop
 docker build -f /home/pi/DeepCultureMonitor/ip-build/Dockerfile . -t ip-loop
-docker run -d --restart=unless-stopped --name ip-loop ip-loop
+docker run -d -v /home/pi/DeepCultureMonitor/data/:/data/ --restart=unless-stopped --name ip-loop ip-loop
 
 if [[ "$CHANGEDDIRS" = true ]]
 then
