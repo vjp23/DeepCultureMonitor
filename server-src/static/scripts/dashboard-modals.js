20ed6b26484d32724c8cd93ed023cbb362d6b1fd
Vince Petaccio <vincepetaccio@gmail.com>
2023-05-09 13:18:28 -0400

    Use controlButtonGrp in ecModalBox class

--- server-src/static/scripts/dashboard-modals.js
+++ server-src/static/scripts/dashboard-modals.js
@@ -75,7 +75,7 @@ class phModalBox {
 				confirmString += `Dispense ${this.downControls.value} mL of pH Down`
 			}
 		};
-		return [requestData, confirmString]
+		return [requestData, confirmString];
 	}
 
 	onShow() {
@@ -91,51 +91,38 @@ class phModalBox {
 
 class ecModalBox {
 	constructor(selector) {
-		this.ecAmounts = [0, 0, 0, 0];
 		this.maxAmounts = [150, 150, 150, 150];
-		this.requestNames = ['nute1', 'nute2', 'nute3', 'nute4', 'nute5'];
+		this.requestNames = ['nute1', 'nute2', 'nute3', 'nute4'];
 		this.displayNames = ['FloraGro', 'FloraMicro', 'FloraBloom', 'CALiMAGic'];
 		this.ecIncrement = 5;
 		this.elem = document.querySelector(selector);
 		this.dispenseButton = document.querySelector('#dispense-ec-button');
 		this.bodyText = document.querySelector('.ec-modal-body p');
-		this.ecOneText = document.querySelector('#ec-one-text');
-		this.ecOnePlus = document.querySelector('#ec-one-plus');
-		this.ecOneMinus = document.querySelector('#ec-one-minus');
-		this.ecTwoText = document.querySelector('#ec-two-text');
-		this.ecTwoPlus = document.querySelector('#ec-two-plus');
-		this.ecTwoMinus = document.querySelector('#ec-two-minus');
-		this.ecThreeText = document.querySelector('#ec-three-text');
-		this.ecThreePlus = document.querySelector('#ec-three-plus');
-		this.ecThreeMinus = document.querySelector('#ec-three-minus');
-		this.ecFourText = document.querySelector('#ec-four-text');
-		this.ecFourPlus = document.querySelector('#ec-four-plus');
-		this.ecFourMinus = document.querySelector('#ec-four-minus');
-		this.textAreas = [this.ecOneText, this.ecTwoText, this.ecThreeText, this.ecFourText];
-		this.plusButtons = [this.ecOnePlus, this.ecTwoPlus, this.ecThreePlus, this.ecFourPlus];
-		this.minusButtons = [this.ecOneMinus, this.ecTwoMinus, this.ecThreeMinus, this.ecFourMinus];
+		this.oneControls = new controlButtonGrp('#ec-one-plus', '#ec-one-minus', '#ec-one-text', 0, this.maxAmounts[0], this.ecIncrement, this.updateDispenseButton.bind(this));
+		this.twoControls = new controlButtonGrp('#ec-two-plus', '#ec-two-minus', '#ec-two-text', 0, this.maxAmounts[1], this.ecIncrement, this.updateDispenseButton.bind(this));
+		this.threeControls = new controlButtonGrp('#ec-three-plus', '#ec-three-minus', '#ec-three-text', 0, this.maxAmounts[2], this.ecIncrement, this.updateDispenseButton.bind(this));
+		this.fourControls = new controlButtonGrp('#ec-four-plus', '#ec-four-minus', '#ec-four-text', 0, this.maxAmounts[3], this.ecIncrement, this.updateDispenseButton.bind(this));
+		this.controls = [this.oneControls, this.twoControls, this.threeControls, this.fourControls];
 		this.setupEvents();
 	}
 	
 	show() {
+		this.controls.forEach((ctl) => {
+			ctl.setValue(0);
+		});
 		this.elem.classList.toggle('is-active');
 		this.onShow()
 	}
 	
 	close() {
-		this.ecAmounts = [0, 0, 0, 0];
-		this.textAreas.forEach((area) => {
-			area.value = '';
-		});
-		this.minusButtons.forEach((btn) => {
-			btn.setAttribute('disabled', 'disabled');
-		});
-		this.plusButtons.forEach((btn) => {
-			btn.removeAttribute('disabled');
-		});
-		this.dispenseButton.setAttribute('disabled', 'disabled');
 		this.elem.classList.toggle('is-active');
 		this.onClose();
+	}
+
+	getRequestedAmounts() {
+		const ecAmounts = [this.oneControls.value, this.twoControls.value, this.threeControls.value, this.fourControls.value];
+		const amountSum = ecAmounts.reduce((partialSum, a) => partialSum + a, 0);
+		return [ecAmounts, amountSum];
 	}
 
 	setupEvents() {
@@ -153,64 +140,10 @@ class ecModalBox {
 			this.close();
 			confirmModal.show(requestData, confirmString);
 		});
-
-	  // + and - buttons
-		this.ecOnePlus.addEventListener("click", () => {
-			this.onEcOnePlus();
-		});
-
-		this.ecOneMinus.addEventListener("click", () => {
-			this.onEcOneMinus();
-		});
-
-		this.ecTwoPlus.addEventListener("click", () => {
-			this.onEcTwoPlus();
-		});
-
-		this.ecTwoMinus.addEventListener("click", () => {
-			this.onEcTwoMinus();
-		});
-
-		this.ecThreePlus.addEventListener("click", () => {
-			this.onEcThreePlus();
-		});
-
-		this.ecThreeMinus.addEventListener("click", () => {
-			this.onEcThreeMinus();
-		});
-
-		this.ecFourPlus.addEventListener("click", () => {
-			this.onEcFourPlus();
-		});
-
-		this.ecFourMinus.addEventListener("click", () => {
-			this.onEcFourMinus();
-		});
-
-		// Text boxes (ensure only numbers)
-		this.ecOneText.addEventListener("blur", () => {
-			this.ecAmounts[0] = validateNumber(this.ecOneText.value);
-			this.updateOne();
-		});
-
-		this.ecTwoText.addEventListener("blur", () => {
-			this.ecAmounts[1] = validateNumber(this.ecTwoText.value);
-			this.updateTwo();
-		});
-
-		this.ecThreeText.addEventListener("blur", () => {
-			this.ecAmounts[2] = validateNumber(this.ecThreeText.value);
-			this.updateThree();
-		});
-
-		this.ecFourText.addEventListener("blur", () => {
-			this.ecAmounts[3] = validateNumber(this.ecFourText.value);
-			this.updateFour();
-		});
 	}
 
 	updateDispenseButton() {
-		const amountSum = this.ecAmounts.reduce((partialSum, a) => partialSum + a, 0);
+		const [ecAmounts, amountSum] = this.getRequestedAmounts();
 		if (amountSum > 0) {
 			this.dispenseButton.removeAttribute('disabled');
 		} else {
@@ -218,132 +151,16 @@ class ecModalBox {
 		}
 	}
 
-	onEcOnePlus() {
-		this.ecAmounts[0] = Math.min(this.maxAmounts[0], this.ecAmounts[0] + this.ecIncrement);
-		this.updateOne();
-	}
-
-	onEcOneMinus() {
-		this.ecAmounts[0] = Math.max(0, this.ecAmounts[0] - this.ecIncrement);
-		this.updateOne();
-	}
-
-	updateOne() {
-	  	// Clip values between 0 and this.maxAmounts[0], enable/disable buttons, etc
-		if (this.ecAmounts[0] > 0) {
-			this.ecAmounts[0] = Math.min(this.ecAmounts[0], this.maxAmounts[0]);
-			if (this.ecAmounts[0] == this.maxAmounts[0]) {
-				this.ecOnePlus.setAttribute('disabled', 'disabled');
-			} else {
-				this.ecOnePlus.removeAttribute('disabled');
-			};
-			this.ecOneMinus.removeAttribute('disabled');
-			this.ecOneText.value = this.ecAmounts[0];
-		} else {
-			this.ecOnePlus.removeAttribute('disabled');
-			this.ecOneMinus.setAttribute('disabled', 'disabled');
-			this.ecOneText.value = '';
-		};
-		this.updateDispenseButton();
-	}
-
-	onEcTwoPlus() {
-		this.ecAmounts[1] = Math.min(this.maxAmounts[1], this.ecAmounts[1] + this.ecIncrement);
-		this.updateTwo();
-	}
-
-	onEcTwoMinus() {
-		this.ecAmounts[1] = Math.max(0, this.ecAmounts[1] - this.ecIncrement);
-		this.updateTwo();
-	}
-
-	updateTwo() {
-	  // Clip values between 0 and this.maxAmounts[1], enable/disable buttons, etc
-		if (this.ecAmounts[1] > 0) {
-			this.ecAmounts[1] = Math.min(this.ecAmounts[1], this.maxAmounts[1]);
-			if (this.ecAmounts[1] == this.maxAmounts[1]) {
-				this.ecTwoPlus.setAttribute('disabled', 'disabled');
-			} else {
-				this.ecTwoPlus.removeAttribute('disabled');
-			};
-			this.ecTwoMinus.removeAttribute('disabled');
-			this.ecTwoText.value = this.ecAmounts[1];
-		} else {
-			this.ecTwoPlus.removeAttribute('disabled');
-			this.ecTwoMinus.setAttribute('disabled', 'disabled');
-			this.ecTwoText.value = '';
-		};
-		this.updateDispenseButton();
-	}
-
-	onEcThreePlus() {
-		this.ecAmounts[2] = Math.min(this.maxAmounts[2], this.ecAmounts[2] + this.ecIncrement);
-		this.updateThree();
-	}
-
-	onEcThreeMinus() {
-		this.ecAmounts[2] = Math.max(0, this.ecAmounts[2] - this.ecIncrement);
-		this.updateThree();
-	}
-
-	updateThree() {
-	  // Clip values between 0 and this.maxAmounts[2], enable/disable buttons, etc
-		if (this.ecAmounts[2] > 0) {
-			this.ecAmounts[2] = Math.min(this.ecAmounts[2], this.maxAmounts[2]);
-			if (this.ecAmounts[2] == this.maxAmounts[2]) {
-				this.ecThreePlus.setAttribute('disabled', 'disabled');
-			} else {
-				this.ecThreePlus.removeAttribute('disabled');
-			};
-			this.ecThreeMinus.removeAttribute('disabled');
-			this.ecThreeText.value = this.ecAmounts[2];
-		} else {
-			this.ecThreePlus.removeAttribute('disabled');
-			this.ecThreeMinus.setAttribute('disabled', 'disabled');
-			this.ecThreeText.value = '';
-		};
-		this.updateDispenseButton();
-	}
-
-	onEcFourPlus() {
-		this.ecAmounts[3] = Math.min(this.maxAmounts[3], this.ecAmounts[3] + this.ecIncrement);
-		this.updateFour();
-	}
-
-	onEcFourMinus() {
-		this.ecAmounts[3] = Math.max(0, this.ecAmounts[3] - this.ecIncrement);
-		this.updateFour();
-	}
-
-	updateFour() {
-	  // Clip values between 0 and this.maxAmounts[3], enable/disable buttons, etc
-		if (this.ecAmounts[3] > 0) {
-			this.ecAmounts[3] = Math.min(this.ecAmounts[3], this.maxAmounts[3]);
-			if (this.ecAmounts[3] == this.maxAmounts[3]) {
-				this.ecFourPlus.setAttribute('disabled', 'disabled');
-			} else {
-				this.ecFourPlus.removeAttribute('disabled');
-			};
-			this.ecFourMinus.removeAttribute('disabled');
-			this.ecFourText.value = this.ecAmounts[3];
-		} else {
-			this.ecFourPlus.removeAttribute('disabled');
-			this.ecFourMinus.setAttribute('disabled', 'disabled');
-			this.ecFourText.value = '';
-		};
-		this.updateDispenseButton();
-	}
-
 	makeRequest() {
 		let requestData = [];
 		let confirmString = '';
 
-		const amountSum = this.ecAmounts.reduce((partialSum, a) => partialSum + a, 0);
+		const [ecAmounts, amountSum] = this.getRequestedAmounts();
 		if (amountSum > 0) {
 		// Do this because the variable `this` is not in scope in the mapped function
 			const requestNames = this.requestNames;
 			const displayNames = this.displayNames;
-			this.ecAmounts.map(function(amount, i) {
+			ecAmounts.map(function(amount, i) {
 				if (amount > 0) {
 					let ecRequest = {"ec": {"status": "request"}};
 					ecRequest["ec"]["action"] = requestNames[i];
@@ -356,7 +173,7 @@ class ecModalBox {
 				}
 			});
 		};
-		return [requestData, confirmString]
+		return [requestData, confirmString];
 	}
 
 	onShow() {
@@ -367,10 +184,6 @@ class ecModalBox {
 	onClose() {
 		const event = new Event('modal:close');
 		this.elem.dispatchEvent(event);
-	}
-
-	addEventListener(event, callback) {
-		this.elem.addEventListener(event, callback);
 	}
 };
 
@@ -442,7 +255,7 @@ class levelModalBox {
 			};
 			requestData.push({"level": {"status": "request", "action": "set", "value": this.controls.value}})
 		};
-		return [requestData, confirmString]
+		return [requestData, confirmString];
 	}
 	
 	onShow() {
@@ -601,15 +414,6 @@ class controlButtonGrp {
 		};
 		this.updateCallback();
 	}
-};
-
-const validNumber = new RegExp(/^\d*\.?\d+/);
-const validateNumber = (newValue) => {
-	if (validNumber.test(newValue)) {
-		return parseFloat(newValue);
-	} else {
-		return 0;
-	};
 };
 
 // Setup modals and enable buttons to open them
